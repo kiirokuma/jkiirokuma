7b537b4e7774a72e53012a83af22fd3e8314e5a4
takenori hirata <kiirokuma@gmail.com>
2019-04-20 00:29:46 +0900

    初回登録

---
+++ jkiirokuma/src/main/java/com/hrt/jkiirokuma/JkiirokumaApplication.java
@@ -0,0 +1,13 @@
+package com.hrt.jkiirokuma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkiirokumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkiirokumaApplication.class, args);
+	}
+
+}
